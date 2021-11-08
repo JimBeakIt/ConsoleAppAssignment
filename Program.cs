764acff9f42e68943ea0f34e331302560074ad6f
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-08 11:14:16 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = new string[5];
+
+            strArray[0] = "feeling happy?";
+            strArray[1] = "feeling sad?";
+            strArray[2] = "feeling cross?";
+            strArray[3] = "feeling excited?";
+            strArray[4] = "feeling annoyed?";
+
+            Console.WriteLine("Are you" + strArray[0]);
+            string happy = Console.ReadLine();
+
+
+        }
+    }
+}
