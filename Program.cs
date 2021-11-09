9516ac53a3a01509593615eadfab66d64a05a52e
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-09 09:11:12 +0000

    Update2

--- Program.cs
+++ Program.cs
@@ -10,45 +10,61 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
-            string[] strArray = new string[5];
+            //string[] strArray = new string[5];
 
-            strArray[0] = "Hello, ";
-            strArray[1] = "Your age is ";
-            strArray[2] = "You live in ";
-            strArray[3] = "You enjoy eating ";
-            strArray[4] = "Your faviorite film is ";
+            //strArray[0] = "Hello, ";
+            //strArray[1] = "Your age is ";
+            //strArray[2] = "You live in ";
+            //strArray[3] = "You enjoy eating ";
+            //strArray[4] = "Your faviorite film is ";
 
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
 
-            Console.WriteLine("How old are you?");
-            string age = Console.ReadLine();
+            //Console.WriteLine("How old are you?");
+            //string age = Console.ReadLine();
 
-            Console.WriteLine("What country do you live in?");
-            string country = Console.ReadLine();
+            //Console.WriteLine("What country do you live in?");
+            //string country = Console.ReadLine();
 
-            Console.WriteLine("What do you enjoy eating?");
-            string eating = Console.ReadLine();
+            //Console.WriteLine("What do you enjoy eating?");
+            //string eating = Console.ReadLine();
 
-            Console.WriteLine("What is your faviorite film?");
-            string film = Console.ReadLine();
+            //Console.WriteLine("What is your faviorite film?");
+            //string film = Console.ReadLine();
 
-            Console.WriteLine("\n" + strArray[0] + name);
+            //Console.WriteLine("\n" + strArray[0] + name);
+            //Console.ReadLine();
+
+            //Console.WriteLine(strArray[1] + age);
+            //Console.ReadLine();
+
+            //Console.WriteLine(strArray[2] + country);
+            //Console.ReadLine();
+
+            //Console.WriteLine(strArray[3] + eating);
+            //Console.ReadLine();
+
+            //Console.WriteLine(strArray[4] + film);
+            //Console.ReadLine();
+
+            int i = 0;
+
+            while (true)
+            {
+                
+
+                Console.WriteLine($"i = {i}");
+                i++;
+
+                if (i > 28)
+                {
+                    break;
+                }
+            }
             Console.ReadLine();
-
-            Console.WriteLine(strArray[1] + age);
-            Console.ReadLine();
-
-            Console.WriteLine(strArray[2] + country);
-            Console.ReadLine();
-
-            Console.WriteLine(strArray[3] + eating);
-            Console.ReadLine();
-
-            Console.WriteLine(strArray[4] + film);
-            Console.ReadLine();
-
-
         }
+
     }
 }
+
