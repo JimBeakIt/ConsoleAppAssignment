a227d8c173123be514b30ba98e9df129b5c4d5cd
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-13 08:30:23 +0000

    update4

--- Program.cs
+++ Program.cs
@@ -10,59 +10,59 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
-            //string[] strArray = new string[5];
+            string[] strArray = new string[5];
 
-            //strArray[0] = "Hello, ";
-            //strArray[1] = "Your age is ";
-            //strArray[2] = "You live in ";
-            //strArray[3] = "You enjoy eating ";
-            //strArray[4] = "Your faviorite film is ";
+            strArray[0] = "Hello, ";
+            strArray[1] = "Your age is ";
+            strArray[2] = "You live in ";
+            strArray[3] = "You enjoy eating ";
+            strArray[4] = "Your faviorite film is ";
 
-            //Console.WriteLine("What is your name?");
-            //string name = Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
 
-            //Console.WriteLine("How old are you?");
-            //string age = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
 
-            //Console.WriteLine("What country do you live in?");
-            //string country = Console.ReadLine();
+            Console.WriteLine("What country do you live in?");
+            string country = Console.ReadLine();
 
-            //Console.WriteLine("What do you enjoy eating?");
-            //string eating = Console.ReadLine();
+            Console.WriteLine("What do you enjoy eating?");
+            string eating = Console.ReadLine();
 
-            //Console.WriteLine("What is your faviorite film?");
-            //string film = Console.ReadLine();
+            Console.WriteLine("What is your faviorite film?");
+            string film = Console.ReadLine();
 
-            //Console.WriteLine("\n" + strArray[0] + name);
-            //Console.ReadLine();
+            Console.WriteLine("\n" + strArray[0] + name);
+            Console.ReadLine();
 
-            //Console.WriteLine(strArray[1] + age);
-            //Console.ReadLine();
+            Console.WriteLine(strArray[1] + age);
+            Console.ReadLine();
 
-            //Console.WriteLine(strArray[2] + country);
-            //Console.ReadLine();
+            Console.WriteLine(strArray[2] + country);
+            Console.ReadLine();
 
-            //Console.WriteLine(strArray[3] + eating);
-            //Console.ReadLine();
+            Console.WriteLine(strArray[3] + eating);
+            Console.ReadLine();
 
-            //Console.WriteLine(strArray[4] + film);
-            //Console.ReadLine();
+            Console.WriteLine(strArray[4] + film);
+            Console.ReadLine();
 
-            //int i = 0;
+            int i = 0;
 
-            //while (true)
-            //{
+            while (true)
+            {
 
 
-            //    Console.WriteLine($"i = {i}");
-            //    i++;
+                Console.WriteLine($"i = {i}");
+                i++;
 
-            //    if (i > 28)
-            //    {
-            //        break;
-            //    }
-            //}
-            //Console.ReadLine();
+                if (i > 28)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
 
             {
                 for (int j = 1; j <= 20; j++)
@@ -71,6 +71,38 @@ namespace ConsoleAppAssignment
                 }
             }
             Console.ReadLine();
+
+            Console.WriteLine("Guess a name");
+
+            List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+            string findName = Console.ReadLine();
+
+            if (names.Contains(findName))
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Guess a fruit");
+
+            List<string> names1 = new List<string>() { "Apple", "Pear", "Peach", "Banana", "Apple", "Orange" };
+            string findFruit = Console.ReadLine();
+
+            if (names1.Contains(findFruit))
+            {
+                Console.WriteLine(names1.IndexOf(findFruit));
+            }
+            else
+            {
+                Console.WriteLine("Not on my list.");
+            }
+            Console.ReadLine();
+
+
         }
 
     }
